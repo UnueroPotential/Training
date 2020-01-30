ecd1cf29f0cbe7d9bfecb02b0284c8ae6d5da257
Unuero Potential <unueroavwerosuo@gmail.com>
2020-01-30 11:23:58 +0100

    Add project files.

---
+++ Training/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Training.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Other> Others { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //SEEDING DATA
+            var roles = new List<IdentityRole>()
+            {
+                new IdentityRole
+                {
+                    Name = "admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name = "staff",
+                    NormalizedName = "STAFF"
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Training/Data/Migrations/20200130092129_NewProject.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Training.Data.Migrations
+{
+    public partial class NewProject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OtherDescription",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "dc52a722-d311-4610-af22-c3533e6eef11", "bd32fe27-5e11-4040-8f54-70bb75bec5e7", "admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "dd6eee67-fe48-4be4-826a-181ea8386c8c", "9ba8e2b6-05a1-4f17-b7e4-baa4d02a164c", "staff", "STAFF" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "dc52a722-d311-4610-af22-c3533e6eef11");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "dd6eee67-fe48-4be4-826a-181ea8386c8c");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "OtherDescription",
+                table: "AspNetUsers");
+        }
+    }
+}
